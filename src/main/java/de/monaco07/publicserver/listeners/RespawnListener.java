a2daae2821fc3187cdc43891052a26d4ecc2164f
monaco07 | Leonard <leomonaco07@gmail.com>
2023-02-06 22:53:47 +0100

    RespawnEvent add EventHandler ?

--- src/main/java/de/monaco07/publicserver/listeners/RespawnListener.java
+++ src/main/java/de/monaco07/publicserver/listeners/RespawnListener.java
@@ -3,10 +3,12 @@ package de.monaco07.publicserver.listeners;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class RespawnListener implements Listener {
+    @EventHandler
     public void onRespawn(PlayerRespawnEvent event) {
         World world = Bukkit.getServer().getWorld("lobby");
         Location spawnLocation = new Location(world, 0, -60, 0);
